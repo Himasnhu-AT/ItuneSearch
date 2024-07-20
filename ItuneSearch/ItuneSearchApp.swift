a3da466b5b5920f481f91581f3f044293af3ec6d
Himanshu-AT <aristech.official@gmail.com>
2024-07-20 21:38:01 +0530

    Initial Commit

---
+++ ItuneSearch/ItuneSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItuneSearchApp.swift
+//  ItuneSearch
+//
+//  Created by Himanshu on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ItuneSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
